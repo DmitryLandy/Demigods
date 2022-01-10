15f979a459661f8d979e60a6100d1485a946ef48
Dmitry Landy <dmitry.landy@outlook.com>
2022-01-10 12:02:06 -0800

    Mostly working in Async

--- Assets/GobanTestScript.cs
+++ Assets/GobanTestScript.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,68 +13,76 @@ public class GobanTestScript : MonoBehaviour
 {
 
     public Text coordinateText;
-    private static int lineCount = 0;
+    public Process process = new Process();
     private static StringBuilder errorOutput = new StringBuilder();
-    public Process process;
+    private static StringBuilder stdOutput = new StringBuilder();
+    private static StreamWriter myStreamWriter;
+    
 
-    private void StartKatagoProcess()
+    private void Start()
     {
-        using (Process process = new Process())
+        // Setup Start info for process
+        ProcessStartInfo startInfo = new ProcessStartInfo()
         {
-            var startInfo = new ProcessStartInfo();
-            startInfo.FileName = $"{Application.dataPath}/katago/katago.exe";
-            startInfo.Arguments = $"gtp -model {Application.dataPath}/katago/b20.bin.gz";
-            startInfo.UseShellExecute = false;
-            startInfo.RedirectStandardOutput = true;
-            startInfo.RedirectStandardInput = true;
-            startInfo.RedirectStandardError = true;
-            startInfo.CreateNoWindow = true;
-
-            process.StartInfo = startInfo;
-
-            process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
-            {
-                var pattern = "^= ([A-S]([1-9]|1[1-9]))$";
-                if (!String.IsNullOrEmpty(e.Data))
-                {
-                    //if (Regex.IsMatch(e.Data, pattern)) coordinateText.text = e.Data; // CAUSES HUNG PROCESS! needs async?
-                    UnityEngine.Debug.Log("\n[" + ++lineCount + "]: " + e.Data);
-                }
-            });
-
-            process.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
-            {
-                // Prepend line numbers to each line of the output.
-                if (!String.IsNullOrEmpty(e.Data))
-                {
-                    errorOutput.Append("\n[" + ++lineCount + "]: " + e.Data);
-                }
-            });
-
-
-            process.Start();
-
-            // Asynchronously read the standard output/Error of the spawned process.
-            // This raises OutputDataReceived events for each line of output.
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-
-            using (StreamWriter myStreamWriter = process.StandardInput)
-            {
-                myStreamWriter.WriteLine("genmove B");
-            }
-
-            process.WaitForExit();
-            //Output any error and stdout
-            UnityEngine.Debug.Log(errorOutput);
-
-        }
-
-
+            FileName = $"{Application.dataPath}/katago/katago.exe",
+            Arguments = $"gtp -model {Application.dataPath}/katago/b20.bin.gz",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardInput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true
+        };
+        process.StartInfo = startInfo;
+    }
+    private void CheckRegex(string data)
+    {
+        var pattern = "^= ([A-S]([1-9]|1[0-9]))$";
+        if (Regex.IsMatch(data, pattern)) UnityEngine.Debug.Log("MATCHED!");
     }
 
-    public void GenMove()
+    public async void GenMove()
     {
-        StartKatagoProcess();
+        process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+        {
+            if (!String.IsNullOrEmpty(e.Data))
+            {
+                CheckRegex(e.Data);
+                stdOutput.Append($"\n{e.Data}");
+            }
+        });
+
+        process.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
+        {
+            if (!String.IsNullOrEmpty(e.Data)) errorOutput.Append($"\n{e.Data}");
+        });
+
+        await Task.Run(() =>
+        {
+            Process[] katagoProcess = Process.GetProcessesByName("katago.exe");
+            if(katagoProcess.Length==0)process.Start();
+            // Asynchronously read the standard output/Error of the spawned process.
+            // This raises OutputDataReceived events for each line of output.
+            
+        });
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        UnityEngine.Debug.Log(process.HasExited);
+        myStreamWriter = process.StandardInput;
+
+        await Task.Run(() =>
+        {
+            
+            myStreamWriter.WriteLine("genmove B");
+            
+        });
+
+        myStreamWriter.WriteLine("quit");
+
+        //process.WaitForExit();
+        //Output any error and stdout
+        UnityEngine.Debug.Log(errorOutput);
+        UnityEngine.Debug.Log(stdOutput);
+        process.Dispose();
     }
 }
